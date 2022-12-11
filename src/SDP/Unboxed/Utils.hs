b0f3c9f9da72ca7902db40958145b2c7cdf0c658
Andrey Mulik <work.a.mulik@gmail.com>
2022-12-11 09:48:41 +0300

    refactor: Unboxed class moved to internal module, actual SDP.Unboxed module API pre-saved for safe refactoring

---
+++ src/SDP/Unboxed/Utils.hs
@@ -0,0 +1,10 @@
+{- |
+    Module      :  SDP.Unboxed.Utils
+    Copyright   :  (c) Andrey Mulik 2019-2022
+    License     :  BSD-style
+    Maintainer  :  work.a.mulik@gmail.com
+    Portability :  non-portable (GHC extensions)
+    
+    "SDP.Unboxed.Utils" provide service definitions for 'Unboxed' class.
+-}
+module SDP.Unboxed.Utils where
