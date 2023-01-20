c25566b9726a49d6ee2024edfd5a0b95baecf2d0
Andrey Mulik <work.a.mulik@gmail.com>
2023-01-20 10:25:09 +0300

    refactor,feat: added toProxy and toProxy1 helpers, fixed debug messages

--- src/SDP/Proxy.hs
+++ src/SDP/Proxy.hs
@@ -11,7 +11,11 @@
 -}
 module SDP.Proxy
 (
-  asProxy#, toProxy#, fromProxy#, fromProxy, unliftProxy##, fromProxy1
+  -- * Proxy
+  fromProxy, toProxy, fromProxy1, toProxy1,
+  
+  -- * Proxy\#
+  toProxy#, fromProxy#, asProxy#, unliftProxy##
 )
 where
 
@@ -24,6 +28,8 @@ default ()
 --------------------------------------------------------------------------------
 
 {- |
+  @since 0.2
+  
   Returns 'undefined' (sdp < 0.3) or 'UnreachableException' (with function name
   for debug, since @sdp-0.3@) of suitable type.
 -}
@@ -31,15 +37,16 @@ fromProxy :: proxy e -> e
 fromProxy =  \ _ -> unreachEx "fromProxy: inappropriate use, (fromProxy e)\
                               \ should never be evaluated."
 
---------------------------------------------------------------------------------
-
 {- |
   @since 0.3
   
-  Returns second argument.
+  Returns 'UnreachableException' (with function name for debug) of suitable type.
 -}
-asProxy# :: Proxy# e -> e -> e
-asProxy# =  \ _ x -> x
+toProxy :: e -> proxy e
+toProxy =  \ _ -> unreachEx "toProxy: inappropriate use, (toProxy e)\
+                           \ should never be evaluated."
+
+--------------------------------------------------------------------------------
 
 {- |
   @since 0.3
@@ -61,6 +68,14 @@ fromProxy# =  \ _ -> unreachEx "fromProxy#: inappropriate use, (fromProxy# e)\
 --------------------------------------------------------------------------------
 
 {- |
+  @since 0.3
+  
+  Returns second argument.
+-}
+asProxy# :: Proxy# e -> e -> e
+asProxy# =  \ _ x -> x
+
+{- |
   Since 0.3
   
   Retuns 'proxy#'.
@@ -77,14 +92,15 @@ unliftProxy## =  \ _ -> proxy#
   for debug, since @sdp-0.3@) of suitable type.
 -}
 fromProxy1 :: m (proxy e) -> e
-fromProxy1 =  \ _ -> unreachEx "fromProxy1: inappropriate use of the @fromProxy1@\
-                              \ function: the value should never be used."
+fromProxy1 =  \ _ -> unreachEx "fromProxy1: inappropriate use, (fromProxy1 e)\
+                              \ should never be evaluated."
+
+toProxy1 :: e -> m (proxy e)
+toProxy1 =  \ _ -> unreachEx "toProxy1: inappropriate use, (toProxy1 e)\
+                            \ should never be evaluated."
 
 --------------------------------------------------------------------------------
 
 unreachEx :: String -> a
 unreachEx =  throw . UnreachableException . showString "SDP.Proxy."
-
-
-
 
